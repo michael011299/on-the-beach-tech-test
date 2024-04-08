8f8ee467db7d334ed83435dbca3269099e82359c
michael011299 <michaelawhitham@hotmail.co.uk>
2024-04-08 15:01:47 +0100

    simplifying/making dry the sorting section

--- src/components/Sorting/Sorting.jsx
+++ src/components/Sorting/Sorting.jsx
@@ -1,31 +1,24 @@
 import React from "react";
 import "./sorting.css";
+import sortingInfo from "../../data/sortingInfo.json";
 
 export const Sorting = ({ handleChange }) => {
+  const sortingData = sortingInfo.sortingInfo;
+
   return (
     <div className='sorting'>
-      <button className='sort-button' onClick={() => handleChange("hotelName")}>
-        sort <strong>alphabetically</strong>
-        <i
-          id='sort-icons'
-          class='fa-solid fa-arrow-up-a-z'
-          style={{ float: "right", fontSize: "large", marginRight: "5%", color: "#c7c7c7" }}
-        ></i>
-      </button>
-      <button className='sort-button' onClick={() => handleChange("price")}>
-        sort by <strong>price</strong>
-        <i
-          class='fa-solid fa-sterling-sign'
-          style={{ float: "right", fontSize: "large", marginRight: "5%", color: "#c7c7c7" }}
-        ></i>
-      </button>
-      <button className='sort-button' onClick={() => handleChange("starRating")}>
-        sort by <strong>star rating</strong>
-        <i
-          class='fa-solid fa-star'
-          style={{ float: "right", fontSize: "large", marginRight: "5%", color: "#c7c7c7" }}
-        ></i>
-      </button>
+      {sortingData.map((sortMethod) => {
+        return (
+          <button className='sort-button' onClick={() => handleChange(sortMethod.sortValue)}>
+            sort <strong>{sortMethod.label}</strong>
+            <i
+              id='sort-icons'
+              class={`fa-solid ${sortMethod.icon}`}
+              style={{ float: "right", fontSize: "large", marginRight: "5%", color: "#c7c7c7" }}
+            ></i>
+          </button>
+        );
+      })}
     </div>
   );
 };
